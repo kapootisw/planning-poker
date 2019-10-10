274a90b91b1993b6e587185c8728e1b743b80a3a
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 10:36:37 -0600

    Use css in button

--- src/common/inputs/Button.js
+++ src/common/inputs/Button.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../css/Input.css'
 
 export const Button = ({ label = '', onClick }) => (
   <div className={'planning-input planning-button'} onClick={onClick}>
