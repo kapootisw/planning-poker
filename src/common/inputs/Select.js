c8d803e39d88dba17bd0e6759f9256902f42fd8f
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 10:41:07 -0600

    Add label to Select

--- src/common/inputs/Select.js
+++ src/common/inputs/Select.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../../css/Input.css'
 
-export const Select = ({ name, onChange, value, options = [] }) => (
+export const Select = ({ name, labelText, onChange, value, options = [] }) => (
   <div className={'planning-input'}>
+    <label htmlFor={name} className={'planning-input-label'}>{labelText}</label>
     <select
       name={name}
       onChange={onChange}
