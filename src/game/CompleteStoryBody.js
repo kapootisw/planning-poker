41d583336147856e05bd10b5c7f19e95448ff777
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 16:39:12 -0600

    Update styles on complete story body

--- src/game/CompleteStoryBody.js
+++ src/game/CompleteStoryBody.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 export default function CompleteStoryBody ({title, notes, score}) {
   return (
-    <div>
-      <h4>{title}</h4>
-      <div>{notes}</div>
-      <div>{score}</div>
+    <div className={'planning-complete-story'}>
+      <div className={'planning-complete-story-text'}>
+        <h4>{title}</h4>
+        <div>{notes}</div>
+      </div>
+      <div className={'planning-complete-score-container'}>
+        <h5>Score</h5>
+        <div className={'planning-complete-story-score'}>{score}</div>
+      </div>
     </div>
   )
 }
