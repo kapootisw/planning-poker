31c2feda01d0116c72a1a3cccfbf8936d3265eb6
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 13:12:07 -0600

    Update CardContainer to be export default function

--- src/game/CardContainer.js
+++ src/game/CardContainer.js
@@ -2,18 +2,20 @@ import React from 'react'
 import '../css/CardContainer.css'
 import { Card } from './Card'
 
-export const CardContainer = ({ cards = [] }) => (
-  <div className={'planning-card-container'}>
-    {
-      cards.map(card => (
-        <Card
-          key={card.id}
-          id={card.id}
-          name={card.name}
-          value={card.value}
-          hidden={card.hidden}
-        />
-      ))
-    }
-  </div>
-)
+export default function CardContainer ({ cards = [] }) {
+  return (
+    <div className={'planning-card-container'}>
+      {
+        cards.map(card => (
+          <Card
+            key={card.id}
+            id={card.id}
+            name={card.name}
+            value={card.value}
+            hidden={card.hidden}
+          />
+        ))
+      }
+    </div>
+  )
+}
