1dc695bec6050270d10fa3c26412185be19db5cc
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 15:59:18 -0600

    Make the complete story body show something

--- src/game/CompleteStoryBody.js
+++ src/game/CompleteStoryBody.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function CompleteStoryBody () {
+export default function CompleteStoryBody ({title, notes, score}) {
   return (
     <div>
+      <h4>{title}</h4>
+      <div>{notes}</div>
+      <div>{score}</div>
     </div>
   )
 }
