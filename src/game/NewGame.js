bc5fa6cec909086f20129749d7f652fd5efbb966
lewiswalther <kapootismaximusrex@gmail.com>
2019-10-10 09:55:03 -0600

    Update NewGame to have moderator name input

--- src/game/NewGame.js
+++ src/game/NewGame.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { TextInput } from '../common/inputs/TextInput'
 
-export default function NewGame () {
-  return (
-    <div className={'newGame'}>
-      This is to make a new game
-    </div>
-  )
+class NewGame extends Component {
+  state = {
+    moderatorName: '',
+    gameName: '',
+    votingValuesChoice: 'fibonocci',
+  }
+
+  updateModeratorName = (e) => {
+    e.preventDefault && e.preventDefault()
+    this.setState({ moderatorName: e.target.value })
+  }
+
+  render = () => {
+    return (
+      <div className={'newGame'}>
+        <h2>Create a new game!</h2>
+        <TextInput
+          name={'moderatorName'}
+          labelText={'Your Name'}
+          onChange={this.updateModeratorName}
+          value={this.state.moderatorName}
+        />
+      </div>
+    )
+  }
 }
+
+export default NewGame
